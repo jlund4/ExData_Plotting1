482994d5d60a21a0ed7ba4c7406de10e9189a86a
jlund4 <jesselund86@gmail.com>
2014-05-10 08:44:16 -0500

    revised

--- plot3.R
+++ plot3.R
@@ -18,6 +18,6 @@ lines(hpc2$Sub_metering_3, type = 'l', col="blue")
 axis(1,label="Thur", at = 0)
 axis(1,label="Fri", at = 1440)
 axis(1,label="Sat", at = 2910)
-legend("topright", lty = c(1,1,1), bty = "n", col = c("black", "red", "blue"),
+legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"),
 legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
